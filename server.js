78747fbea151b6e19dfff84ba9f22823ea2b6471
shilo <haz.shilo@gmail.com>
2023-10-16 14:39:20 +0300

    make it better

--- server.js
+++ server.js
@@ -18,11 +18,36 @@ app.use(cors());
   const db = connection.db("mydb1");
   collection = db.collection("products");
 })();
+(async () => {
+  const url =
+    "mongodb+srv://hazshilo:1234@cluster1.ifbyw.mongodb.net/?tlsAllowInvalidCertificates=true";
+  const connection = await mongo.connect(url);
+  const db = connection.db("mydb1");
+  collectionU = db.collection("users-and-pratim");
+})();
 
 app.get("/", async (req, res) => {
   res.json(await collection.find({}).toArray());
 });
+app.get("/findPritim", async (req, res) => {
+  let data = await collectionU.find({}).toArray();
+  res.send(data);
+});
+app.post("/SendData", async (req, res) => {
+  // console.log(req.body.products);
+  await collectionU.insertMany([req.body]);
+  res.json("ok");
+});
+app.delete("/delco", async (req, res) => {
+  await collectionU.deleteMany({});
+  res.json("ok");
+});
+app.delete("/Delhazmana/:id", async (req, res) => {
+  let id = req.params.id;
+  await collectionU.deleteOne({ _id: new ObjectId(id) });
 
+  res.json("ok");
+});
 app.listen(port, () => {
   console.log(`http://localhost:${port}/`);
 });
